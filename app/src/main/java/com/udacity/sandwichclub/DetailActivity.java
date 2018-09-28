d588e0d489d700fe2a1c5f6e781366439664783d
yaostyle <yaostyle@gmail.com>
2018-09-28 00:24:05 -0700

    Add comments to codes and minor clean up.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,42 +3,41 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
-import java.util.ArrayList;
-
 public class DetailActivity extends AppCompatActivity {
-
+    // Init vars
+    // Define static position to identify if there's data received
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    // Create private TextViews
     private TextView mainNameTextView;
     private TextView alsoKnownAsTextView;
     private TextView placeOfOriginTextView;
     private TextView descriptionTextView;
     private TextView ingredientsTextView;
 
-    private static final String TAG = "DetailActivity";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        // Connect image's View
         ImageView ingredientsIv = findViewById(R.id.image_iv);
 
+        // If there's no intent, show error and close
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
         }
 
+        // Retrieve intent extra data
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
@@ -46,60 +45,76 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
+        // Create a sandwiches string var from string array
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
+        // Create a json var from the position that was received from intent extra
         String json = sandwiches[position];
+        // Parse json data
         Sandwich sandwich = JsonUtils.parseSandwichJson(json, getApplicationContext());
+
+        // If there's no sandwich data, show error and close
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
             return;
         }
 
+        // Populate UI views
         populateUI(sandwich);
 
+        // Grab the image using Picasso
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
 
+        // Set the current title based on current sandwich name
         setTitle(sandwich.getMainName());
     }
 
+    // method to close the activity and make a Toast message
     private void closeOnError() {
         finish();
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
+    // method to populate ui views
     private void populateUI(Sandwich sandwichData) {
-
+        // Connect all views by its id
         mainNameTextView = findViewById(R.id.tv_main_name);
         alsoKnownAsTextView = findViewById(R.id.tv_also_known_as);
         placeOfOriginTextView = findViewById(R.id.tv_place_of_origin);
         descriptionTextView = findViewById(R.id.tv_description);
         ingredientsTextView = findViewById(R.id.tv_ingredients);
 
+        // Concatenating aka list into a single string
         String akaList = "";
         int akaDataCount = sandwichData.getAlsoKnownAs().size();
         for (int i=0; i<akaDataCount; i++) {
             akaList += sandwichData.getAlsoKnownAs().get(i);
+            // If current position is in between the list,
+            // add a comma for readability
             if (i>0 && i<akaDataCount-1){
                 akaList += ", ";
             }
         }
 
+        // Concatenating ingredient list into a single string
         String ingredientList = "";
         int ingCount = sandwichData.getIngredients().size();
         for (int x=0; x<ingCount; x++) {
             ingredientList += sandwichData.getIngredients().get(x);
+            // If current position is in between the list,
+            // add a comma for readability
             if (x>0 && x<ingCount-1) {
                 ingredientList += ", ";
             }
         }
 
+        // Set current data to the views
         mainNameTextView.setText(sandwichData.getMainName());
         alsoKnownAsTextView.setText(akaList);
         placeOfOriginTextView.setText(sandwichData.getPlaceOfOrigin());
         descriptionTextView.setText(sandwichData.getDescription());
         ingredientsTextView.setText(ingredientList);
-
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,6 +1,7 @@
 package com.udacity.sandwichclub.utils;
 
 import android.content.Context;
+
 import com.udacity.sandwichclub.R;
 import com.udacity.sandwichclub.model.Sandwich;
 import org.json.JSONArray;
@@ -11,8 +12,10 @@ import java.util.ArrayList;
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json, Context context) {
+        // Init vars
         Sandwich parsedSandwichData = new Sandwich();
 
+        // Create json key vars
         final String KEY_MAIN_NAME          = context.getString(R.string.json_key_main_name);
         final String KEY_NAME               = context.getString(R.string.json_key_name);
         final String KEY_ALSO_KNOWN_AS      = context.getString(R.string.json_key_also_known_as);
@@ -21,36 +24,52 @@ public class JsonUtils {
         final String KEY_IMAGE              = context.getString(R.string.json_key_image);
         final String KEY_INGREDIENTS        = context.getString(R.string.json_key_ingredients);
 
+        // Parsing json string
         try {
+            // Create a JSONObject from supplied json string
             JSONObject sandwichJsonBaseObj  = new JSONObject(json);
+            // Create a JSONObject for "name" key
             JSONObject sandwichJsonNamesObj = sandwichJsonBaseObj.getJSONObject(KEY_NAME);
+            // Extract mainName string
             String mainNameString           = sandwichJsonNamesObj.getString(KEY_MAIN_NAME);
+            // Create a JSONArray for alsoKnownAs
             JSONArray alsoKnownAsArr        = sandwichJsonNamesObj.getJSONArray(KEY_ALSO_KNOWN_AS);
+            // Extract placeOfOrigin string
             String placeOfOriginString      = sandwichJsonBaseObj.getString(KEY_PLACE_OF_ORIGIN);
+            // Extract description string
             String descriptionString        = sandwichJsonBaseObj.getString(KEY_DESCRIPTION);
+            // Extract image string
             String imageString              = sandwichJsonBaseObj.getString(KEY_IMAGE);
+            // Create a JSONArray for ingredients
             JSONArray ingredientsArr        = sandwichJsonBaseObj.getJSONArray(KEY_INGREDIENTS);
 
+            // Create a new ArrayList for alosKnownAs
             ArrayList<String> alsoKnownAsList = new ArrayList<>();
+            // Iterate through the json array and add it to ArrayList
             if (alsoKnownAsArr.length() > 0) {
                 for (int i = 0; i < alsoKnownAsArr.length(); i++) {
                     alsoKnownAsList.add(alsoKnownAsArr.getString(i));
                 }
             } else {
+                // If json array is empty, set a default value called N/A
                 alsoKnownAsList.add("N/A");
             }
 
+            // Create a new ArrayList for ingredient
             ArrayList<String> ingredientsList = new ArrayList<>();
+            // Loop through the json array and add it to ArrayList
             if (ingredientsArr.length() > 0) {
                 for (int x = 0; x < ingredientsArr.length(); x++) {
                     ingredientsList.add(ingredientsArr.getString(x));
                 }
             }
 
+            // If placeOfOrigin is empty, set a default value called Unknow
             if (placeOfOriginString.isEmpty()) {
                 placeOfOriginString = "Unknow";
             }
 
+            // Gather all the data and set to parsedSandwichData
             parsedSandwichData = new Sandwich(mainNameString
                                                 , alsoKnownAsList
                                                 , placeOfOriginString
@@ -59,9 +78,11 @@ public class JsonUtils {
                                                 , ingredientsList);
 
         } catch (JSONException e) {
+            // If there's JSON exception, print to stack
             e.printStackTrace();
         }
 
+        // Return the final Sandwich data
         return parsedSandwichData;
     }
 
